64178c580a620e94ed8e22252f3592fec0e71a28
Gabriel Genê <gabrielgene97@gmail.com>
2016-11-23 07:39:01 -0300

    Create websocket connection

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var app = require('./config/server');
 
-app.listen(8000, function(){
-  console.log("Server chat run in localhost:8000");
+var server = app.listen(8000, function(){
+  console.log("Server run in localhost:8000");
+});
+
+var io = require('socket.io').listen(server);
+
+io.on('connection', function(socket){
+  console.log('User on');
+  socket.on('disconnect', function(){
+    console.log('User off');
+  })
 });
